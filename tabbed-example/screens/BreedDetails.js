12dec62eb5c833339b562c16fd627abf946cfb26
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-12 10:34:04 -0800

    trying to import and route

--- tabbed-example/screens/BreedDetails.js
+++ tabbed-example/screens/BreedDetails.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {View, Text, ScrollView} from 'react-native';
+import breedData from '../cat-and-dog-breeds.json'
+
 
 function BreedDeatils(){
     return (
         <ScrollView style ={{padding:20}}>
 
+            <Text>
+
+            </Text>
 
 
 
 
 
 
-            
         </ScrollView>
     )
 }
