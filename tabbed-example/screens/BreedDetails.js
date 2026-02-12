27e6e1dbe8cf52deb1d7972da409b7c0befeeacd
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-12 12:59:33 -0800

    Update BreedDetails.js

--- tabbed-example/screens/BreedDetails.js
+++ tabbed-example/screens/BreedDetails.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import {View, Text, ScrollView} from 'react-native';
-import breedData from '../cat-and-dog-breeds.json'
+import breedData from '../cat-and-dog-breeds.json';
+
 
 
 function BreedDeatils(){
+    const {name } = route.params
+    const traits = breedData.cat_breeds[name] || breedData.dog_breeds[name];
+
     return (
-        <ScrollView style ={{padding:20}}>
-
-            <Text>
-
+        <ScrollView style={{ padding: 20 }}>
+          <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 15 }}>{name}</Text>
+          {Object.entries(traits).map(([trait, value]) => (
+            <Text key={trait} style={{ fontSize: 16, paddingVertical: 5 }}>
+              {trait}: {value}
             </Text>
-
-
-
-
-
-
+          ))}
         </ScrollView>
-    )
+      );
 }
