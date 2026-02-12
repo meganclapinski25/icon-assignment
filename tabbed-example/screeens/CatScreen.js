ae88979e2f53ebf6e59dceb0ccd7ab3360553db9
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-12 10:22:23 -0800

    error fixed

--- tabbed-example/screeens/CatScreen.js
+++ tabbed-example/screeens/CatScreen.js
@@ -28,3 +28,5 @@ function CatScreen() {
       </View>
     );
   }
+
+  export default CatScreen;
--- tabbed-example/screeens/DogScreen.js
+++ tabbed-example/screeens/DogScreen.js
@@ -28,3 +28,5 @@ function DogScreen() {
       </View>
     );
   }
+
+  export default DogScreen;
